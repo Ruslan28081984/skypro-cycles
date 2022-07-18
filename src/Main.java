4f01bf39d94353e7b9b4211b030591e8dc434c45
Ruslan Mustafin <89201828020@mail.ru>
2022-07-18 21:37:13 +0300

    почистил код

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-//     for (int i = 2020; i < 2070; i = i + 4 ){
-//         System.out.println(" високосный год: " + i);
-//     }
         int salary = 65535;
         int total = 0;
         int i = 0;
         for (; total < 10_000_000; i ++) {
-            total = total + total / 100;
-            total = total + salary;
+            total = total + total / 100 + salary;
             if (i % 5 == 0) {
                 System.out.println("Месяц " + i + " итого " + total);
             }
